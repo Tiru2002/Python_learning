1fa9c0726006033856284b143514f8f935c46316
Tiru2002 <tirumalarao1805@gmail.com>
2024-02-19 23:13:31 +0530

    loading day06

---
+++ 06_Day_String/String.py
@@ -0,0 +1,27 @@
+#singel line string
+a='this is string'
+print(a)
+
+#multiple lines string
+b=""" i'm a student in information  technology department 
+      in anna university """
+print(b)
+c=''' this is also multiple lines string
+      method  of defining multiple lines strings '''
+print(c)
+
+# a string is a  sequence of character array we can access length of it by index
+d='hello_String'
+print(len(d))
+
+#accessing the characters using index
+print("index 0 is",d[0])
+print( "index -1 is ", d[-1] )
+
+#slicing :
+print( 'slicing:',d[2:5])
+print( 'reversed slicing:',d[::-1])
+
+
+
+
